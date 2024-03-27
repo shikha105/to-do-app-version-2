341a8bf3c03ec6ba7f8675dd4c5bf4df4a855534
shikha105 <shikha105789@gmail.com>
2024-03-27 14:26:09 -0700

    using the hook 'useRef' so that it doesn't re-render everything everytime

--- src/components/TodoItemAdd.jsx
+++ src/components/TodoItemAdd.jsx
@@ -2,25 +2,16 @@ import { useState, useRef } from "react";
 import { IoMdAddCircle } from "react-icons/io";
 
 function TodoItemAdd({ onNewItem }) {
-  const [todoName, setTodoName] = useState("");
-  const [dueDate, setDueDate] = useState("");
-  const noOfUpdates = useRef(0);
-
-  const handleNameChange = (event) => {
-    setTodoName(event.target.value);
-    noOfUpdates.current++;
-  };
-
-  const handleDateChange = (event) => {
-    setDueDate(event.target.value);
-    console.log(`no of updates: ${noOfUpdates.current}`);
-  };
+  const todoNameElement = useRef();
+  const dueDateElement = useRef();
 
   const handleAddButtonClicked = (event) => {
     event.preventDefault();
+    const todoName = todoNameElement.current.value;
+    const dueDate = dueDateElement.current.value;
+    todoNameElement.current.value = "";
+    dueDateElement.current.value = "";
     onNewItem(todoName, dueDate);
-    setTodoName("");
-    setDueDate("");
   };
 
   return (
@@ -28,18 +19,13 @@ function TodoItemAdd({ onNewItem }) {
       <form onSubmit={handleAddButtonClicked} className="row kg-row">
         <div className="col-6">
           <input
+            ref={todoNameElement}
             type="text"
-            value={todoName}
             placeholder="Enter todo here"
-            onChange={handleNameChange}
           ></input>
         </div>
         <div className="col-4">
-          <input
-            type="date"
-            value={dueDate}
-            onChange={handleDateChange}
-          ></input>
+          <input ref={dueDateElement} type="date"></input>
         </div>
         <div className="col-2">
           <button className="btn btn-success kg-button">
